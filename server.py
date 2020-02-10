ec50ec03c49a58e0abfe58a7a187dbef886129fa
Eriq <eriqdev@gmail.com>
2020-02-10 14:01:56 +0300

    Connect and Save data to postgresql database in server

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket
+import json
+import psycopg2
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('socket succesfuly created')
 serv.bind(('localhost', 12345))
@@ -13,7 +16,7 @@ while True:
         if not data:
             break
 
-        file = open("rawData", "wb")
+        file = open("rawData.json", "wb")
         file.write(data)
         file.close()
         conn.sendall(b"I am SERVER\n")
@@ -21,4 +24,27 @@ while True:
     print ('client disconnected')
 
 
+""" Connect to the PostgreSQL database server """
+con = None
 
+print('Connecting to the PostgreSQL database...')
+try:
+    con = psycopg2.connect(host="localhost",database="fetal_hrm", user="postgres", password="123")
+    cursor = con.cursor()
+
+    with open('rawData.json', 'r') as client_data:
+        client_dict = json.load(client_data)
+
+    for data_object in client_dict:
+        sql = """INSERT INTO monitor_readings(uuid,height,weight,temperature,heart_rate,client_created) VALUES(%s,%s,%s,%s,%s,%s);"""
+        data = (data_object['uuid'], data_object['height'], data_object['weight'], data_object['temperature'], data_object['heart_rate'], data_object['date_created'])
+        cursor.execute(sql,data)
+        con.commit()
+    print('Data saved succesfuly')
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+        if con is not None:
+            con.close()
+            print('Database connection closed.')
