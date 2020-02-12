2036c0541b817cee2b3d2e49c0429c15a9aabf0b
Eriq <eriqdev@gmail.com>
2020-02-12 16:54:18 +0300

    Modified server db to allow easy synchronization

--- sql/server-postgres.sql
+++ sql/server-postgres.sql
@@ -1,5 +1,6 @@
 CREATE TABLE maternal_information (
 	maternal_ID SERIAL PRIMARY KEY,
+	uuid TEXT UNIQUE,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	date_of_birth DATE NOT NULL,
@@ -10,12 +11,12 @@ CREATE TABLE maternal_information (
 
 CREATE TABLE pregnancy_information (
 	pregnancy_ID SERIAL PRIMARY KEY,
-	maternal_ID INTEGER NOT NULL,
+	maternal_uuid TEXT NOT NULL,
 	location TEXT NOT NULL,
 	pregnancy_type TEXT DEFAULT 'single',
 	expected_delivery_date DATE NOT NULL,
 	pregnancy_count TEXT NOT NULL,
-	FOREIGN KEY (maternal_ID) REFERENCES maternal_information(maternal_ID) ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY (maternal_uuid) REFERENCES maternal_information(uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE monitor_readings (
